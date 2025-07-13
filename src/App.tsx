3a63e892ac30cf3dfa9430523b334b8b45cf1d01
Chen Peleg <chenpeleg@gmail.com>
2025-07-13 13:18:58 +0300

    added readme

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {Component} from './component/Component.tsx';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+        <Component/>
       <div>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} className={"logo flex"} alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
---
+++ src/component/Component.tsx
@@ -0,0 +1,7 @@
+import styles from './Component.module.css'
+
+export const Component = () => {
+    return <div className={styles.a}>
+
+    </div>
+}
